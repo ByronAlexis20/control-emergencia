68ed55ae36100cf5a06a204794c0300ebd0a3b0f
ByronAlexis20 <byronalexisramirez20@gmail.com>
2021-08-19 23:17:38 -0500

    bomberos

--- src/com/emergencia/control/registros/BomberoLista.java
+++ src/com/emergencia/control/registros/BomberoLista.java
@@ -1,5 +1,60 @@
 package com.emergencia.control.registros;
 
-public class BomberoLista {
+import java.io.IOException;
+import java.util.List;
 
+import org.zkoss.bind.annotation.AfterCompose;
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.ContextParam;
+import org.zkoss.bind.annotation.ContextType;
+import org.zkoss.bind.annotation.GlobalCommand;
+import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.select.Selectors;
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zk.ui.util.Clients;
+import org.zkoss.zul.ListModelList;
+import org.zkoss.zul.Listbox;
+
+import com.emergencia.model.dao.UsuarioDAO;
+import com.emergencia.model.entity.Usuario;
+
+public class BomberoLista {
+	public String textoBuscar;
+	List<Usuario> listaBombero;
+	@Wire private Listbox lstBomberos;
+	UsuarioDAO usuarioDAO = new UsuarioDAO();
+	
+	@AfterCompose
+	public void aferCompose(@ContextParam(ContextType.VIEW) Component view) throws IOException{
+		Selectors.wireComponents(view, this, false);
+		textoBuscar="";
+		buscar();
+	}
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@GlobalCommand("Barrio.buscarPorPatron")
+	@Command
+	@NotifyChange({"listaBombero"})
+	public void buscar(){
+		if (listaBombero != null) {
+			listaBombero = null; 
+		}
+		listaBombero = barrioDAO.getBarrioPorDescripcion(textoBuscar);
+		lstBomberos.setModel(new ListModelList(listaBombero));
+		if(listaBombero.size() == 0) {
+			Clients.showNotification("No hay datos para mostrar.!!");
+		}
+	}
+	public List<Usuario> getListaBombero() {
+		return listaBombero;
+	}
+	public void setListaBombero(List<Usuario> listaBombero) {
+		this.listaBombero = listaBombero;
+	}
+	public String getTextoBuscar() {
+		return textoBuscar;
+	}
+	public void setTextoBuscar(String textoBuscar) {
+		this.textoBuscar = textoBuscar;
+	}
 }
--- src/com/emergencia/util/Globales.java
+++ src/com/emergencia/util/Globales.java
@@ -4,4 +4,13 @@ public class Globales {
 	public static String codigoPrehospitalaria = "APH";
 	public static String codigoControlIncendio = "CI";
 	public static String codigoLaborSocial = "LS";
+	/***********************************************************
+	 * Codigos de los tipos de perfiles
+	 ***********************************************************/
+	public static Integer codigoAdministrador = 1;
+	public static Integer codigoSecretaria = 2;
+	public static Integer codigoJefeGuardia = 3;
+	public static Integer codigoBomberoRentado = 4;
+	public static Integer codigoChofer = 5;
+	public static Integer codigoBomberoVoluntario = 6;
 }
